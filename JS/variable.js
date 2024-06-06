6300a11f8ced32b23433f311865394a7ac3ec477
Chirag <95977259+Chirag26mahajan@users.noreply.github.com>
2024-06-06 18:00:37 +0000

    variable constants

---
+++ JS/variable.js
@@ -0,0 +1,12 @@
+// const number = 1323;
+// let name = "chirag";
+// var place = "shimla";
+// var count = "134";
+// address = "delhi";
+
+// // name = "chigs";
+// console.table([number,name,place,address]);
+
+var b =20
+var b = "stringg"
+console.log(b)
