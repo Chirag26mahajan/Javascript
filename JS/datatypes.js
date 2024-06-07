b0e5fb5c649d7b51925690be02447fb0bd1d251d
Chirag <95977259+Chirag26mahajan@users.noreply.github.com>
2024-06-07 06:33:53 +0000

    datatypes

---
+++ JS/datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treal all js codes to the newer version
+//number=> 2 to power 53
+// bigint ---> ye use hota hai jese un sites me bitcoins , value bhaut zada ho
+// null=> standalone value 
+//undefined --> let state; abhi ye hua undefined
+//symbol --> uniquness 
+//object
+//console.log(typeof  );
+//typeof null is object whereas that of undefined is undefined only
+ 
+
--- JS/variable.js
+++ JS/variable.js
@@ -7,6 +7,26 @@
 // // name = "chigs";
 // console.table([number,name,place,address]);
 
-var b =20
-var b = "stringg"
-console.log(b)
+// var b =20
+// var b = "stringg"
+// console.log(b)
+
+//Difference between var and let
+// ----------------------difference for var-----------------
+// function example1(){
+//    if(true){
+//     var x = 10;
+//    }
+//    console.log(x);
+// }
+// example1();
+
+//-----------------DIfference for let-------------------
+function example2(){
+    if(true){
+        let x = 19;
+    }
+    console.log(x);
+}
+
+example2();
