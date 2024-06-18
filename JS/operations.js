bb116f8e6cdd4ea75cb43cb774c7ef50451e66c6
Chirag <95977259+Chirag26mahajan@users.noreply.github.com>
2024-06-18 04:40:43 +0000

    operation.js

--- JS/operations.js
+++ JS/operations.js
@@ -1,4 +1,8 @@
 //****************Operations *****************/
-console.log(1+"2"); //12
-console.log(2+"1"); //21
-console.log("1"+2+2); //122
+// console.log(1+"2"); //12
+// console.log(2+"1"); //21
+// console.log("1"+2+2); //122
+
+let name;
+console.log(typeof name);
+console.log(+"");
