d1e3d749b9bfe551679d8ec1e0e7a3b2dcd46bee
Chirag <95977259+Chirag26mahajan@users.noreply.github.com>
2024-06-18 17:24:32 +0000

    loose and strict

--- JS/operations.js
+++ JS/operations.js
@@ -2,7 +2,16 @@
 // console.log(1+"2"); //12
 // console.log(2+"1"); //21
 // console.log("1"+2+2); //122
-
+// console.log(1+2+"2");
 let name;
-console.log(typeof name);
+// console.log(typeof name);
 console.log(+"");
+// console.log("2">1);
+
+// console.log(null >0);    false
+// console.log(null ==0); false
+// console.log(null >=0); true
+let num1 = 1;
+let str = "1";
+
+console.log(num1===str);
