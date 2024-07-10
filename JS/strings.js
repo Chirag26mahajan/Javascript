8f141d39c5898acc198433f385a78ca1f72cff72
Chirag <95977259+Chirag26mahajan@users.noreply.github.com>
2024-07-10 15:57:45 +0000

    concepts of math

--- JS/strings.js
+++ JS/strings.js
@@ -17,4 +17,20 @@ const url = "https://chirag.com/chirag%20mahajan"
 
 
 const nameTwo = "hello everyone my name is chirag";
-console.log(nameTwo.split(" ",4));
+// console.log(nameTwo.split(" "));
+const score = 400
+const balance = new Number(100); //important
+// console.log(balance); //output is [Number: 100]
+// console.log(balance.toString().length); //3
+// console.log(balance.toFixed(2)); //100.00  for precision value
+
+const otherNum = 23.899;
+// console.log(otherNum.toPrecision(3)); 23.9
+const newNum = 123.943;
+console.log(newNum.toPrecision(3)); //123
+
+const hundred = 1000000
+// console.log(hundred.toLocaleString()); //1,000,000 accoring to US standards --- to coonvert it into indian standards we write 
+//("en-IN")
+// console.log(hundred.toLocaleString("en-IN"));
+console.log(Math.random()); //iski value 0 se 1 ke beech me he ayegi
