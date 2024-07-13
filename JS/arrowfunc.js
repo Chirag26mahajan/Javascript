ede37f7f9cb2445c56fc820aef7abd5ae62f349f
Chirag <95977259+Chirag26mahajan@users.noreply.github.com>
2024-07-13 13:01:09 +0000

    arrow function

--- JS/arrowfunc.js
+++ JS/arrowfunc.js
@@ -11,3 +11,17 @@ const user= {
 // user.weclomeMessage()
 // user.username = "abhay"
 // user.weclomeMessage()
+
+
+const chai  = function(){
+    let username = "chirag"
+    console.log(this);
+}
+// chai()
+
+//Arrow Function
+const addTwoNum = (num1,num2)=>num1+num2
+// console.log(addTwoNum(3,5));
+
+const naming = (num1,num2) =>({usrname:"chirag"})
+console.log(naming(3,4));
---
+++ JS/iife.js
@@ -0,0 +1,9 @@
+(function chai(name){
+    console.log(`DB connected ${name}`);
+})("abhaay");
+
+
+((name)=>{
+    console.log(`Db connected now ${name}`)
+})("chirag")
+    
