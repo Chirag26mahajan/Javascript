5df3990af8600aa45311e84654e1b63c047af9e0
Chirag <95977259+Chirag26mahajan@users.noreply.github.com>
2024-07-15 06:59:39 +0000

    callstack

--- JS/iife.js
+++ JS/iife.js
@@ -1,9 +1,36 @@
-(function chai(name){
-    console.log(`DB connected ${name}`);
-})("abhaay");
+// (function chai(name){
+//     console.log(`DB connected ${name}`);
+// })("abhaay");
 
 
-((name)=>{
-    console.log(`Db connected now ${name}`)
-})("chirag")
+// ((name)=>{
+//     console.log(`Db connected now ${name}`)
+// })("chirag")
+    // var a; 
+    // console.log(a);
+
+    // let b 
+    // console.log(b);
+
     
+    // console.log(c);
+name();
+function name(){
+    console.log("my name is chirag");
+}
+
+// console.log(a);
+// let a=10;
+
+
+console.log(10);
+
+function name() {
+    console.log("chirag");
+}
+
+setTimeout(function() {
+    name();
+}, 2000);
+
+console.log(20);
