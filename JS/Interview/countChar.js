9b2afa91f039d5bc21617afb9b15be545c474a42
Chirag <chiragmahajan26.cm@gmail.com>
2024-08-27 15:40:59 +0530

    countcharacters

---
+++ JS/Interview/countChar.js
@@ -0,0 +1,12 @@
+const str = "Hello People, how are u doing ??"
+
+const obj ={}
+for(let x of str){
+    if(obj[x]){
+        obj[x]+=1
+    }else{
+        obj[x] = 1;
+    }
+}
+
+console.log(obj)
