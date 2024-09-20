bf96a026d2d8d68817399147e291d9f56e6ce651
Chirag <chiragmahajan26.cm@gmail.com>
2024-09-20 23:29:32 +0530

    Ques

--- JS/Interview/basics.js
+++ JS/Interview/basics.js
@@ -270,13 +270,13 @@ console.log(y); //2
 //Avoid Polluting global scope
 (()=>{
 //Synatx
-})();
-
-(function(){
-    let str = "CHirag";
-    console.log(str);
-    
-})();
+})();  
 // console.log(str);  will show an error that str is not defined refernceError
 
 
+//QuesInput: a = "11", b = "1"
+// Output: "100"  
+var addBinary = function(a, b) {
+    let sum = BigInt('0b'+a)+BigInt('0b'+b)
+    return sum.toString(2)
+};
